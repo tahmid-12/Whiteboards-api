e88f503a97dfd87413e6db1b1cf82f305bcbf52c
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2024-09-15 00:35:32 +0600

    Second push

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const drawingRoutes = require('./routes/drawingRoutes');
 const PORT = 3000;
 
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost:27017/whiteboard').then(() => console.log('MongoDB connected'))
